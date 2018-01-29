0157225bf5a86b6fed7b3568402f4ed6052d5daa
Dani Vijay <danimvijay@gmail.com>
2018-01-29 09:46:02 +0530

    responsive nav display, add theme

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     temporary
     v-model="drawer"
     absolute
+    class="hidden-sm-and-up"
     >
       <v-list>
         <v-list-tile @click="">
@@ -16,13 +17,18 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar>
-      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon
+        @click="drawer = !drawer"
+        class="hidden-sm-and-up"
+        ></v-toolbar-side-icon>
       <v-toolbar-title>
         The Meetup App
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items
+        class="hidden-xs-only"
+        >
         <v-btn flat>
           <v-icon left>group</v-icon>
           View Meetups
--- src/main.js
+++ src/main.js
@@ -4,7 +4,14 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#E64A19',
+    secondary: '#FF5722',
+    accent: '#FF9800',
+    error: '#b71c1c'
+  }
+})
 
 Vue.config.productionTip = false
 
